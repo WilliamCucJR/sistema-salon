d0abc7f40a8509a891bf3820fb297d0f47901714
Jose Lopez <114269446+JoseMLG04@users.noreply.github.com>
2024-09-30 22:47:59 -0600

    Boton Whatsapp e Instagram

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { Icon, Grid } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
 import './Home.css'; // CSS personalizado
 
 const Home = () => {
@@ -48,9 +49,80 @@ const Home = () => {
           </Link>
         </Grid.Column>
       </Grid>
+        <InstagramButton
+                href="https://www.instagram.com/cul4s_de_freefire/?hl=es-la"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png"
+                    alt="Ver nuestro negocio en Instagram"
+                />
+            </InstagramButton>
+            <WhatsappButton
+                href="https://wa.me/50254050579?text=HOLA"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+                    alt="Chat on WhatsApp"
+                />
+            </WhatsappButton>
     </div>
   );
 };
+
+const pulseAnimation = keyframes`
+    0% {
+        transform: scale(1);
+    }
+    50% {
+        transform: scale(1.1);
+    }
+    100% {
+        transform: scale(1);
+    }
+`;
+
+const InstagramButton = styled.a`
+    position: fixed;
+    bottom: 100px;
+    right: 20px;
+    z-index: 1000;
+    animation: ${pulseAnimation} 1.5s ease-in-out infinite;
+
+    img {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        transition: transform 0.3s ease-in-out;
+
+        &:hover {
+            transform: scale(1.1);
+        }
+    }
+`;
+
+const WhatsappButton = styled.a`
+    position: fixed;
+    bottom: 20px; 
+    right: 20px;
+    z-index: 1000;
+
+    img {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        transition: transform 0.3s ease-in-out;
+
+        &:hover {
+            transform: scale(1.1);
+        }
+    }
+`;
 
 export default Home;
 
